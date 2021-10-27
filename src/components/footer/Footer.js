63568773d5f2fcda158f8e58987744c8b52f7272
villu <viola.smirnova2015@yandex.ru>
2021-10-27 16:54:19 +0300

    correcting header and footer parts without footer-list

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -5,16 +5,49 @@ export function Footer() {
         <div class="footer">
         <div class="footer_row">
             <div class="footer_first-contacts footer_item">
-                <a class="footer_header" href="file:///C:/Users/Xiaomi/Desktop/personal-website/contacts.html">Get in touch</a>
+                {/* <a class="footer_header" href="file:///C:/Users/Xiaomi/Desktop/personal-website/contacts.html">Get in touch</a> */}
+                <div class="footer_header">Get in touch</div>
                 <div class="footer_text">Here is the portfolio of my projects.</div>
-                <button type="button" class="footer_meeting footer_button">MEET WITH ME</button>
-                <button type="button" class="footer_github footer_button">PROJECTS</button>
+                <div class="footer_buttons">
+                    <button type="button" class="footer_meeting footer_button">MEET WITH ME</button>
+                    <button type="button" class="footer_github footer_button">PROJECTS</button>
+                </div>
             </div>
             <div class="footer_other-contacts footer_item">
-                <div class="footer_emailing">Email</div>
-                <div class="footer_linkedin">LinkedIn</div>
-                <div class="footer_resume">Resume</div>
-                <div class="footer_github">Github</div> 
+                <ol>
+                    <a class="footer_list_item" href="mailto:viloloshka8@gmail.com">
+                        <div >
+                            <li class="footer_list">
+                                ::before
+                                "Email"
+                            </li>
+                        </div>
+                    </a>
+                    <a class="footer_list_item" href="">
+                        <div >
+                            <li class="footer_list">
+                                ::before
+                                "LinkedIn"
+                            </li>
+                        </div>
+                    </a>
+                    <a class="footer_list_item" href="">
+                        <div>
+                            <li class="footer_list">
+                                ::before
+                                "Resume"
+                            </li>
+                        </div>
+                    </a>
+                    <a class="footer_list_item" href="">
+                        <div>
+                            <li class="footer_list">
+                                ::before
+                                "Github"
+                            </li> 
+                        </div>
+                    </a>
+                </ol>
             </div>
         </div>
       </div> 
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,20 +1,21 @@
 import './Styles.css'
 
 export function Header({setPage}) {
-    return (
-        <div class="header"> 
-        <div class="header_row nav_bar">
-          <div class="nav_left">
-            <button type="button" onClick={() => setPage('home')} class="nav_main nav_buttons">Viola Zy</button>
-          </div>
-          <button type="button" class="nav_burger" id="nav_click"></button>
-          <div class="nav_right" id="nav_r">
-            <button type="button" onClick={() => setPage('about')} class="nav_about nav_buttons">About</button>
-            <button type="button" onClick={() => setPage('projects')} class="nav_projects nav_buttons">Projects</button>
-            <button type="button" onClick={() => setPage('blog')} class="nav_blog nav_buttons">Blog</button>
-            <button type="button" onClick={() => setPage('contacts')} class="nav_contact nav_buttons">Contact</button>
-          </div>
+  return (
+    <div class="header"> 
+      <div class="header_row nav_bar">
+        <div class="nav_left">
+          {/* <NavigationButton setPage={setPage} pageName='home' buttonClass="nav_main nav_buttons" text="Viola Zy" /> */}
+          <button type="button" onClick={() => setPage('home')} class="nav_main nav_buttons">Viola Zy</button>
+        </div>
+        <button type="button" class="nav_burger" id="nav_click"></button>
+        <div class="nav_right" id="nav_r">
+          <button type="button" onClick={() => setPage('about')} class="nav_about nav_buttons">About</button>
+          <button type="button" onClick={() => setPage('projects')} class="nav_projects nav_buttons">Projects</button>
+          <button type="button" onClick={() => setPage('blog')} class="nav_blog nav_buttons">Blog</button>
+          <button type="button" onClick={() => setPage('contacts')} class="nav_contact nav_buttons">Contact</button>
         </div>
       </div>
-    )
+    </div>
+  )
 }
